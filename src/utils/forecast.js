3d3de7718b999af611ca330226f6711dd7dd5f82
Guillaume ELHADI <guillaume.elhadi@gmail.com>
2020-05-12 12:04:01 +0200

    Resolve github security alert

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,6 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Impossible de trouver la meteo de cette localisation.', undefined)
         } else {
-            console.log(JSON.stringify(body))
             callback(undefined, body.daily.data[0].summary + ' La température actuelle est de ' + body.currently.temperature + ' degrés. La temperature minimale prévue ce jour sera de ' + body.daily.data[0].temperatureMin + ' degrés et la temperature maximale ' + body.daily.data[0].temperatureMax + ' degrés. Il y a ' + body.currently.precipProbability + '% de chance de pluie.')
         }
     })
